a81b69e8d0e30d52ebad508335dad84b9627a19f
Ben Kimani <benzoic09@gmail.com>
2023-10-18 10:15:45 -0700

    create push

---
+++ push.c
@@ -0,0 +1,41 @@
+#include "monty.h"
+
+/**
+ * push - Pushes an element onto the stack.
+ * @stack: Double pointer to the head of the stack.
+ * @line_number: Line number where the push opcode is encountered.
+ */
+void push(stack_t **stack, unsigned int line_number)
+{
+	int value = 0;
+
+	if (!stack)
+	{
+		fprintf(stderr, "L%u: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	stack_t *new_node = malloc(sizeof(stack_t));
+
+	if (!new_node)
+	{
+		 fprintf(stderr, "Error: malloc failed\n");
+		 exit(EXIT_FAILURE);
+	}
+
+	if (!value)
+	{
+		 fprintf(stderr, "L%u: usage: push integer\n", line_number);
+		 free(new_node);
+		 exit(EXIT_FAILURE);
+	}
+
+	new_node->n = value;
+	new_node->next = *stack;
+	new_node->prev = NULL;
+	
+	if (*stack)
+		 (*stack)->prev = new_node;
+
+	*stack = new_node;
+}
