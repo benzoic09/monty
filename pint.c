2030c7d5ed7b0189405c5c6abefc5fdceccd3ba9
steve9948 <mbuguagichimu@gmail.com>
2023-10-19 13:06:34 -0700

    Added pint

--- pint.c
+++ pint.c
@@ -8,11 +8,11 @@
  */
 void f_pint(stack_t **head, unsigned int line_number)
 {
-    if (*head == NULL)
-    {
-        fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-
-    printf("%d\n", (*head)->n);
+	if (*head == NULL)
+	{
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	
+	printf("%d\n", (*head)->n);
 }
