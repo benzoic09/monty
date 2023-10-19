52b0add00caeb907e6b06bb30b5024a0ae05d08a
steve9948 <mbuguagichimu@gmail.com>
2023-10-19 14:07:15 -0700

    emoved merge conflicts

--- pop.c
+++ pop.c
@@ -1,34 +1,14 @@
 #include "monty.h"
 
 /**
-<<<<<<< HEAD
- * pop - Removes the top element from the stack.
- * @stack: Double pointer to the head of the stack.
- * @line_number: Line number where the pop opcode is encountered.
- * Return: no return
-=======
  * f_pop - Removes the top element of the stack.
  * @head: Double pointer to the head of the stack.
  * @line_number: Line number in the file.
->>>>>>> 902c94c0e32b9f7159ce6dceb42d8d8a7cdf865f
  */
 void f_pop(stack_t **head, unsigned int line_number)
 {
     stack_t *temp;
 
-<<<<<<< HEAD
-	if (*head == NULL)
-	{
-		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
-		exit(EXIT_FAILURE);
-		free_stack(*head);
-	}
-
-
-		temp = *head;
-		*head = temp->next;
-		 free(temp);
-=======
     if (*head == NULL)
     {
         fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
@@ -47,5 +27,4 @@ void f_pop(stack_t **head, unsigned int line_number)
         free(*head);
         *head = NULL;
     }
->>>>>>> 902c94c0e32b9f7159ce6dceb42d8d8a7cdf865f
 }
