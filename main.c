8043c1854ab35569d98d249f407ef7a85f654698
steve9948 <mbuguagichimu@gmail.com>
2023-10-20 13:07:24 -0700

    Edited main function

--- main.c
+++ main.c
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
 
 	file = fopen(argv[1], "r");
 
-	if (!is_number && !file (line))
+	if (!is_number(line))
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
