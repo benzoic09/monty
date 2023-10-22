9348b7973172950704978b6e5a65796dbd49819c
steve9948 <mbuguagichimu@gmail.com>
2023-10-22 11:42:16 -0700

    Edited the main to assert the string operations

--- main.c
+++ main.c
@@ -13,29 +13,47 @@ int main(int argc, char *argv[])
 	FILE *file;
 	unsigned int line_number = 0;
 
+	/* Checking the number of arguments */
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
 
+	/* Opening the file */
 	file = fopen(argv[1], "r");
-
-	if (!is_number(line))
+	if (file == NULL)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 
-	while (fgets(line, MAX_LEN, file) != NULL)
+	/* Processing instructions from the file */
+	while (fgets(line, sizeof(line), file))
 	{
 		line_number++;
-		process_instruction(&stack, line, line_number);
+
+		/* Removing trailing newline */
+		if (line[strlen(line) - 1] == '\n')
+			line[strlen(line) - 1] = '\0';
+
+		/* Skipping empty lines or lines starting with spaces */
+		if (strcmp(line, "") == 0 || line[0] == ' ')
+			continue;
+
+		/* Handling given instructions */
+		if (!process_instruction(&stack, line, line_number))
+		{
+			fprintf(stderr, "L%u: usage: push integer\n", line_number);
+			free_stack(stack);
+			fclose(file);
+			exit(EXIT_FAILURE);
+		}
 	}
 
-
+	/* Closing the file and freeing memory */
 	fclose(file);
 	free_stack(stack);
 
-	return (EXIT_SUCCESS);
+	return EXIT_SUCCESS;
 }
