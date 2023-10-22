4410b0de7a7fbe7e23b7b3a56a52a53583b0c944
steve9948 <mbuguagichimu@gmail.com>
2023-10-22 12:05:25 -0700

    Updated the push function to handle string operations

--- push.c
+++ push.c
@@ -11,7 +11,7 @@ void push(stack_t **stack, unsigned int line_number)
 	stack_t *new_node;
 	char *value_str = strtok(NULL, " \n\t");
 
-	if (!value_str)
+	if (!value_str || !is_number(value_str))
 	{
 		fprintf(stderr, "L%u: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
@@ -20,44 +20,41 @@ void push(stack_t **stack, unsigned int line_number)
 	value = atoi(value_str);
 
 	new_node = malloc(sizeof(stack_t));
-
-		if (!new_node)
-		{
-			fprintf(stderr, "Error: malloc failed\n");
-			exit(EXIT_FAILURE);
-		}
-
-		if (!value)
-		{
-			fprintf(stderr, "L%u: usage: push integer\n", line_number);
-			free(new_node);
-			exit(EXIT_FAILURE);
-		}
+	if (!new_node)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
 
 	new_node->n = value;
 	new_node->next = *stack;
 	new_node->prev = NULL;
 
-		if (*stack)
-			(*stack)->prev = new_node;
+	if (*stack)
+		(*stack)->prev = new_node;
 
 	*stack = new_node;
 }
 
+/**
+ * is_number - Checks if a string represents a valid number.
+ * @str: String to be checked.
+ * Return: 1 if the string is a valid number, 0 otherwise.
+ */
 int is_number(char *str)
 {
-    if (!str)
-        return 0;
+	if (!str)
+		return 0;
 
-    if (*str == '-')
-        str++;
+	if (*str == '-')
+		str++;
 
-    while (*str)
-    {
-        if (!isdigit(*str))
-            return 0;
-        str++;
-    }
+	while (*str)
+	{
+		if (!isdigit(*str))
+			return 0;
+		str++;
+	}
 
-    return 1;
+	return 1;
 }
